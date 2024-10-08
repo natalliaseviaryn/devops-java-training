6549a76d83d90681bbb4d5110e3a1ba1843877b4
nseviaryn <nseviaryn@jetsmarter.com>
2024-10-08 20:07:27 +0300

    Added docker for ubuntu ec2

--- terraformEc2/user-data.sh
+++ terraformEc2/user-data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Installation of nginx"
 
-sudo apt-get update -y
+sudo apt-get update
 sudo apt-get install -y nginx
 sudo systemctl start nginx
 sudo systemctl enable nginx
@@ -26,3 +26,16 @@ font-family: Tahoma, Verdana, Arial, sans-serif; }
 </body>
 </html>
 EOF
+
+echo "Installation of docker from https://docs.docker.com/engine/install/ubuntu/"
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
