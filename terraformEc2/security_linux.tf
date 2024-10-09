25ccafd754553f11e4bd2e9e517dc5e533f07006
nseviaryn <nseviaryn@jetsmarter.com>
2024-10-09 16:00:46 +0300

    Linux is visible only for ubuntu ec2

--- terraformEc2/security_linux.tf
+++ terraformEc2/security_linux.tf
@@ -1,15 +1,16 @@
 resource "aws_security_group" "linux_instance_sg" {
   name        = "linux_instance_sg"
   description = "EC2 Amazon Linux should not have access to the Internet, but must have outgoing and incoming access: ICMP, TCP/22, TCP/80, TCP/443 only on the local network where EC2 Ubuntu, EC2 Amazon Linux is located."
-  vpc_id            = data.aws_vpc.default.id
-  tags = {
+  vpc_id      = data.aws_vpc.default.id
+  tags        = {
     Name = "linux_instance_sg"
   }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "linux_allow_icmp" {
   security_group_id = aws_security_group.linux_instance_sg.id
-  cidr_ipv4         = data.aws_vpc.default.cidr_block
+  #  cidr_ipv4         = data.aws_vpc.default.cidr_block # task 3
+  cidr_ipv4         = "${aws_instance.ubuntu_instance.private_ip}/32" # task 7
   from_port         = -1
   to_port           = -1
   ip_protocol       = "icmp"
@@ -17,7 +18,8 @@ resource "aws_vpc_security_group_ingress_rule" "linux_allow_icmp" {
 
 resource "aws_vpc_security_group_ingress_rule" "linux_allow_tcp_22" {
   security_group_id = aws_security_group.linux_instance_sg.id
-  cidr_ipv4         = data.aws_vpc.default.cidr_block
+  #  cidr_ipv4         = data.aws_vpc.default.cidr_block # task 3
+  cidr_ipv4         = "${aws_instance.ubuntu_instance.private_ip}/32" # task 7
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
@@ -26,7 +28,8 @@ resource "aws_vpc_security_group_ingress_rule" "linux_allow_tcp_22" {
 
 resource "aws_vpc_security_group_ingress_rule" "linux_allow_tcp_80" {
   security_group_id = aws_security_group.linux_instance_sg.id
-  cidr_ipv4         = data.aws_vpc.default.cidr_block
+  #  cidr_ipv4         = data.aws_vpc.default.cidr_block # task 3
+  cidr_ipv4         = "${aws_instance.ubuntu_instance.private_ip}/32" # task 7
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
@@ -34,7 +37,8 @@ resource "aws_vpc_security_group_ingress_rule" "linux_allow_tcp_80" {
 
 resource "aws_vpc_security_group_ingress_rule" "linux_allow_tcp_443" {
   security_group_id = aws_security_group.linux_instance_sg.id
-  cidr_ipv4         = data.aws_vpc.default.cidr_block
+  #  cidr_ipv4         = data.aws_vpc.default.cidr_block # task 3
+  cidr_ipv4         = "${aws_instance.ubuntu_instance.private_ip}/32" # task 7
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
@@ -42,7 +46,8 @@ resource "aws_vpc_security_group_ingress_rule" "linux_allow_tcp_443" {
 
 resource "aws_vpc_security_group_egress_rule" "linux_allow_outgoing_icmp" {
   security_group_id = aws_security_group.linux_instance_sg.id
-  cidr_ipv4         = data.aws_vpc.default.cidr_block
+  #  cidr_ipv4         = data.aws_vpc.default.cidr_block # task 3
+  cidr_ipv4         = "${aws_instance.ubuntu_instance.private_ip}/32" # task 7
   from_port         = -1
   to_port           = -1
   ip_protocol       = "icmp"
@@ -50,7 +55,8 @@ resource "aws_vpc_security_group_egress_rule" "linux_allow_outgoing_icmp" {
 
 resource "aws_vpc_security_group_egress_rule" "linux_allow_outgoing_tcp_22" {
   security_group_id = aws_security_group.linux_instance_sg.id
-  cidr_ipv4         = data.aws_vpc.default.cidr_block
+  #  cidr_ipv4         = data.aws_vpc.default.cidr_block # task 3
+  cidr_ipv4         = "${aws_instance.ubuntu_instance.private_ip}/32" # task 7
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
@@ -59,7 +65,8 @@ resource "aws_vpc_security_group_egress_rule" "linux_allow_outgoing_tcp_22" {
 
 resource "aws_vpc_security_group_egress_rule" "linux_allow_outgoing_tcp_80" {
   security_group_id = aws_security_group.linux_instance_sg.id
-  cidr_ipv4         = data.aws_vpc.default.cidr_block
+  #  cidr_ipv4         = data.aws_vpc.default.cidr_block # task 3
+  cidr_ipv4         = "${aws_instance.ubuntu_instance.private_ip}/32" # task 7
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
@@ -67,7 +74,8 @@ resource "aws_vpc_security_group_egress_rule" "linux_allow_outgoing_tcp_80" {
 
 resource "aws_vpc_security_group_egress_rule" "linux_allow_outgoing_tcp_443" {
   security_group_id = aws_security_group.linux_instance_sg.id
-  cidr_ipv4         = data.aws_vpc.default.cidr_block
+  #  cidr_ipv4         = data.aws_vpc.default.cidr_block # task 3
+  cidr_ipv4         = "${aws_instance.ubuntu_instance.private_ip}/32" # task 7
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
