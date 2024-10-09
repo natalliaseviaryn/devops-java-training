a732f39a63cf9067ef52c04c0ccd055a6a03f635
nseviaryn <nseviaryn@jetsmarter.com>
2024-10-09 14:45:49 +0300

    Made AMI IDs aren't hardcoded

--- terraformEc2/main.tf
+++ terraformEc2/main.tf
@@ -6,22 +6,62 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "ubuntu_instance_2" {
-  ami           = "ami-0866a3c8686eaeeba"
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+data "aws_ami" "linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+resource "aws_instance" "ubuntu_instance" {
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
   vpc_security_group_ids = [aws_security_group.ubuntu_instance_sg.id]
 
   user_data_replace_on_change = true
-  user_data = file("user-data.sh")
+  user_data                   = file("user-data.sh")
 
-  tags      = {
+  tags = {
     Name = "training-terraform-ubuntu"
   }
 }
 
-resource "aws_instance" "linux_instance_2" {
-  ami           = "ami-0fff1b9a61dec8a5f"
+resource "aws_instance" "linux_instance" {
+  ami           = data.aws_ami.linux.id
   instance_type = "t2.micro"
 
   vpc_security_group_ids = [
